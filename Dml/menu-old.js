156a38fe1e139080dd2982ad9351b5433a2b3a8a
MLILA05 <dmlemegency1234@gmail.com>
2026-05-04 10:39:46 -0700

    Update menu-old.js

--- Dml/menu-old.js
+++ Dml/menu-old.js
@@ -70,7 +70,7 @@ const menuCaption = `╭━〔 𝗧𝗘𝗦𝗟𝗔-𝗫𝗣𝗔𝗖𝗘 〕━�
         let sentMsg;
         try {
             sentMsg = await conn.sendMessage(from, {
-                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/xksplb.jpg' },
+                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/ydvgry.png' },
                 caption: menuCaption,
                 contextInfo: contextInfo
             }, { quoted: mek });
