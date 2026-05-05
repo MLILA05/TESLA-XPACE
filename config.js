6c6600fb9d7566116e844650b6b75d4fb34dbde4
MLILA05 <dmlemegency1234@gmail.com>
2026-05-05 11:21:44 -0700

    Update config.js

--- config.js
+++ config.js
@@ -60,6 +60,28 @@ module.exports = {
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",        // Show bot as always online?
     AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true", // VIEW to status updates?
     AUTO_BIO: process.env.AUTO_BIO || "false", // ture to get auto bio 
+    AUTOBIO_THEMES: [
+        (d, t, botname) => `⚡ ${botname}\n𝗜 𝗗𝗼𝗻'𝘁 𝗦𝗹𝗲𝗲𝗽, 𝗜 𝗚𝗿𝗶𝗻𝗱 • ${d} ${t}`,
+        (d, t, botname) => `🌙 ${botname}\n𝗤𝘂𝗶𝗲𝘁 𝗕𝘂𝘁 𝗔𝗹𝘄𝗮𝘆𝘀 𝗛𝗲𝗿𝗲 • ${d} ${t}`,
+        (d, t, botname) => `👑 ${botname}\n𝗕𝗼𝘀𝘀 𝗠𝗼𝗱𝗲 𝗔𝗰𝘁𝗶𝘃𝗮𝘁𝗲𝗱 🔛 ${d} ${t}`,
+        (d, t, botname) => `🔥 ${botname}\n𝗪𝗮𝘁𝘂 𝗡𝗶 𝗠𝘁𝗮𝗷𝗶 𝗧𝗼𝘀𝗵𝗮 • 𝗧𝗮𝗻𝘇𝗮𝗻𝗶𝗮 ${d} ${t}`,
+        (d, t, botname) => `🌌 ${botname}\n𝗢𝘂𝘁 𝗢𝗳 𝗧𝗵𝗶𝘀 𝗪𝗼𝗿𝗹𝗱 🚀 ${d} ${t}`,
+        (d, t, botname) => `😎 ${botname}\n𝗨𝗻𝗯𝗼𝘁𝗵𝗲𝗿𝗲𝗱 & 𝗢𝗻𝗹𝗶𝗻𝗲 • ${d} ${t}`,
+        (d, t, botname) => `🌃 ${botname}\n𝗨𝗽 𝗪𝗵𝗲𝗻 𝗧𝗵𝗲 𝗪𝗼𝗿𝗹𝗱 𝗦𝗹𝗲𝗲𝗽𝘀 • ${d} ${t}`,
+        (d, t, botname) => `🧊 ${botname}\n𝗖𝗼𝗼𝗹, 𝗖𝗮𝗹𝗺 & 𝗔𝗹𝘄𝗮𝘆𝘀 𝗢𝗻 • ${d} ${t}`,
+        (d, t, botname) => `💯 ${botname}\n𝗡𝗼 𝗖𝗮𝗽, 𝗜'𝗺 𝗔𝗹𝘄𝗮𝘆𝘀 𝗢𝗻𝗹𝗶𝗻𝗲 • ${d} ${t}`,
+        (d, t, botname) => `🇹🇿 ${botname}\n𝗧𝗮𝗻𝘇𝗮𝗻𝗶𝗮'𝘀 𝗙𝗶𝗻𝗲𝘀𝘁 𝗕𝗼𝘁 • ${d} ${t}`,
+        (d, t, botname) => `💪 ${botname}\n𝗙𝗮𝗹𝗹 𝟳 𝗧𝗶𝗺𝗲𝘀, 𝗥𝗶𝘀𝗲 𝟴 • ${d} ${t}`,
+        (d, t, botname) => `🌠 ${botname}\n𝗗𝗿𝗲𝗮𝗺 𝗕𝗶𝗴, 𝗪𝗼𝗿𝗸 𝗛𝗮𝗿𝗱, 𝗦𝘁𝗮𝘆 𝗛𝘂𝗺𝗯𝗹𝗲 • ${d} ${t}`,
+        (d, t, botname) => `🎯 ${botname}\n𝗦𝗺𝗮𝗹𝗹 𝗦𝘁𝗲𝗽𝘀 𝗦𝘁𝗶𝗹𝗹 𝗠𝗼𝘃𝗲 𝗬𝗼𝘂 𝗙𝗼𝗿𝘄𝗮𝗿𝗱 • ${d} ${t}`,
+        (d, t, botname) => `✨ ${botname}\n𝗕𝗲𝗹𝗶𝗲𝘃𝗲 𝗜𝘁, 𝗕𝘂𝗶𝗹𝗱 𝗜𝘁, 𝗕𝗲𝗰𝗼𝗺𝗲 𝗜𝘁 • ${d} ${t}`,
+        (d, t, botname) => `🔥 ${botname}\n𝗣𝗮𝗶𝗻 𝗜𝘀 𝗧𝗲𝗺𝗽𝗼𝗿𝗮𝗿𝘆, 𝗚𝗿𝗲𝗮𝘁𝗻𝗲𝘀𝘀 𝗜𝘀 𝗙𝗼𝗿𝗲𝘃𝗲𝗿 • ${d} ${t}`,
+        (d, t, botname) => `🚧 ${botname}\n𝗧𝗵𝗲 𝗥𝗼𝗮𝗱 𝗧𝗼 𝗦𝘂𝗰𝗰𝗲𝘀𝘀 𝗜𝘀 𝗔𝗹𝘄𝗮𝘆𝘀 𝗨𝗻𝗱𝗲𝗿 𝗖𝗼𝗻𝘀𝘁𝗿𝘂𝗰𝘁𝗶𝗼𝗻 • ${d} ${t}`,
+        (d, t, botname) => `🛠️ ${botname}\n𝗘𝘃𝗲𝗿𝘆 𝗗𝗮𝘆 𝗜 𝗣𝗮𝘃𝗲 𝗠𝘆 𝗪𝗮𝘆 𝗧𝗼 𝗚𝗿𝗲𝗮𝘁𝗻𝗲𝘀𝘀 • ${d} ${t}`,
+        (d, t, botname) => `🔨 ${botname}\n𝗚𝗿𝗼𝘄𝘁𝗵 𝗜𝘀 𝗔 𝗖𝗼𝗻𝘀𝘁𝗮𝗻𝘁 𝗪𝗼𝗿𝗸𝗶𝗻𝗴 𝗦𝗶𝘁𝗲 • ${d} ${t}`,
+        (d, t, botname) => `🏗️ ${botname}\n𝗕𝘂𝗶𝗹𝗱𝗶𝗻𝗴 𝗠𝘆 𝗗𝗿𝗲𝗮𝗺𝘀 𝗕𝗿𝗶𝗰𝗸 𝗕𝘆 𝗕𝗿𝗶𝗰𝗸 • ${d} ${t}`,
+        (d, t, botname) => `🚀 ${botname}\n𝗧𝗵𝗲 𝗝𝗼𝘂𝗿𝗻𝗲𝘆 𝗜𝘀 𝗡𝗲𝘃𝗲𝗿 𝗙𝗶𝗻𝗶𝘀𝗵𝗲𝗱, 𝗔𝗻𝗱 𝗧𝗵𝗮𝘁'𝘀 𝗧𝗵𝗲 𝗕𝗲𝗮𝘂𝘁𝘆 • ${d} ${t}`,
+    ],
     WELCOME: process.env.WELCOME || "false", // true to get welcome in groups 
     GOODBYE: process.env.GOODBYE || "false", // true to get goodbye in groups 
     ADMIN_ACTION: process.env.ADMIN_ACTION || "false", // true if want see admin activity 
